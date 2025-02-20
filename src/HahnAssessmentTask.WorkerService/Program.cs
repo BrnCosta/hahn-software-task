1712df705b925c807f3ab009fbe0e7bcac9eb10d
BrnCosta <brunooc.bc@gmail.com>
2025-02-19 21:11:21 -0300

    Initial commit

---
+++ src/HahnAssessmentTask.WorkerService/Program.cs
@@ -0,0 +1,7 @@
+using HahnAssessmentTask.WorkerService;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
