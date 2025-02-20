9923427eec980a83e4585c69e7a086e467c4ed98
BrnCosta <brunooc.bc@gmail.com>
2025-02-19 21:52:45 -0300

    Creating service and unit of work interfaces

---
+++ src/HahnAssessmentTask.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using HahnAssessmentTask.Core.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HahnAssessmentTask.Core.Interfaces
+{
+  public interface IUnitOfWork
+  {
+    IStockRepository StockRepository { get; }
+    IStockDailyInformation stockDailyInformation { get; }
+    Task Commit();
+  }
+}
